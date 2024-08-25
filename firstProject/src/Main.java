b049e86490535a787e9ad372958b10a7800587fc
hanbo1116 <3408595565qq.com>
2024-08-25 14:25:02 +0800

    啥也没改

--- firstProject/src/Main.java
+++ firstProject/src/Main.java
@@ -2,7 +2,8 @@
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("hello world111 ");
+
+        System.out.println("hello world111 fffhanbo");
     }
 
 }
